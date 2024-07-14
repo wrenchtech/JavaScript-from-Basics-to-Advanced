175359cc6e6bf01458314940bb94aa2451594b3b
kuro <miguel.albasanchez@gmail.com>
2024-07-14 13:42:45 -0600

    s2_9 terminado

---
+++ JS Course/Sección 2/S2_9.js
@@ -0,0 +1,9 @@
+// Null and Undefined
+
+console.clear()
+
+var mVar
+console.log(mVar) //Sale "undefined" ya que la variable no tiene un valor
+
+mVar = null //La variable tiene el valor nulo "null" para representar que no tiene valor
+console.log(mVar)
