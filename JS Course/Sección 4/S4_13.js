edbbd77bc34ddbfc16e8fc496b5b5a23bc1f8c66
kuro <miguel.albasanchez@gmail.com>
2024-07-15 12:06:22 -0600

    s4_13 terminado

---
+++ JS Course/Sección 4/S4_13.js
@@ -0,0 +1,29 @@
+//Operadores de asignación
+
+console.clear()
+
+var num1 = 15;
+var num2 = 30;
+
+// =
+num1 = 20; //Asigna un valor diferente a una variable
+console.log(num1)
+
+// +=
+num1 += 10; //Le suma al valor de la variable
+console.log(num1)
+
+// -=
+num1 -= 15; //Le resta al valor de una variable
+console.log(num1)
+
+// *=
+num1 *= 10; //Le multiplica al valor de una variable
+console.log(num1)
+
+// /=
+num1 /= 10; //Le divide al valor de una variable
+console.log(num1)
+
+num1 %= 10 //Convierte el valor de la variable al residuo de la division
+console.log(num1) 
